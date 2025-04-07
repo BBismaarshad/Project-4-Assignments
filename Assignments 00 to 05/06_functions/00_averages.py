63d5811ffa771e6757f239d5edea7a484519e9b6
Bisma Arshad <164316452+BBismaarshad@users.noreply.github.com>
2025-04-07 21:54:26 +0500

    Add files via upload

---
+++ Assignments 00 to 05/06_functions/00_averages.py
@@ -0,0 +1,13 @@
+def main():
+    # Prompt the user to enter two numbers
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    
+    # Calculate the average
+    average = (num1 + num2) / 2
+    
+    # Print the result
+    print("The average is:", average)
+
+if __name__ == '__main__':
+    main()

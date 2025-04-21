acecfab0add412129c606b0c0242d3f16b25a6ea
Bisma Arshad <164316452+BBismaarshad@users.noreply.github.com>
2025-04-21 21:36:03 +0500

    Update main.py

--- Assignments 01/01_basics/00_joke_bot.py/main.py
+++ Assignments 01/01_basics/00_joke_bot.py/main.py
@@ -1,4 +1,5 @@
 # Messages
+
 PROMPT: str = "What do you want? "
 JOKE: str = "Here is a joke for you! Sophia is heading out to the grocery store. A programmer tells her: get a liter of milk, and if they have eggs, get 12. Sophia returns with 13 liters of milk. The programmer asks why and Sophia replies: 'because they had eggs'"
 SORRY: str = "Sorry I only tell jokes."

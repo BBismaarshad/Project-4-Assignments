f1f7ed58da48e889d99ef7a5ffe22adb66051e8a
Bisma Arshad <164316452+BBismaarshad@users.noreply.github.com>
2025-05-25 22:52:58 +0500

    Add files via upload

---
+++ OOP_Practice_Series/Series_20.py
@@ -0,0 +1,18 @@
+# Custom Exception
+class InvalidAgeError(Exception):
+    def __init__(self, message="Age must be 18 or older."):
+        self.message = message
+        super().__init__(self.message)
+
+# Function to check age
+def check_age(age):
+    if age < 18:
+        raise InvalidAgeError(f"Invalid age: {age}. You must be 18 or older.")
+    else:
+        print("Age is valid.")
+
+# Testing
+try:
+    check_age(16)
+except InvalidAgeError as e:
+    print("Caught an exception:", e)
